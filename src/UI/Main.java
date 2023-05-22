f145394da9c3675f4f004e8efdb78fd1e5dc5dc2
khalix4 <jeremiah.abu@binghamuni.edu.ng>
2023-05-22 14:04:22 +0100

    first commit

---
+++ src/UI/Main.java
@@ -0,0 +1,25 @@
+//import UI.BankGUI;
+//import UI.CurrentAccount;
+//import UI.SavingsAccount;
+//
+//import javax.swing.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+
+
+
+
+
+public class
+Main {
+    public static void main(String[] args) {
+//       SwingUtilities.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+        new JFrame();
+        UI.WelcomePage welcomePage = new UI.WelcomePage();
+        Frame.getFrames();
+
+    }
+}
