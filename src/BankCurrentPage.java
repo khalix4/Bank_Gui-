18202af4ab78792f8eac026819ca92f1c2e85154
khalix4 <jeremiah.abu@binghamuni.edu.ng>
2023-05-22 15:24:44 +0100

    added a current

---
+++ src/BankCurrentPage.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class BankCurrentPage {
+    private double balance;
+
+    public BankCurrentPage() {
+        balance = 0.0;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds!");
+        }
+    }
+
+    public void displayBalance() {
+        System.out.println("Current balance: " + balance);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BankCurrentPage bank = new BankCurrentPage();
+
+        while (true) {
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Display Balance");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the amount to deposit: ");
+                    double depositAmount = scanner.nextDouble();
+                    bank.deposit(depositAmount);
+                    System.out.println("Amount deposited successfully.");
+                    break;
+
+                case 2:
+                    System.out.print("Enter the amount to withdraw: ");
+                    double withdrawAmount = scanner.nextDouble();
+                    bank.withdraw(withdrawAmount);
+                    System.out.println("Amount withdrawn successfully.");
+                    break;
+
+                case 3:
+                    bank.displayBalance();
+                    break;
+
+                case 4:
+                    System.out.println("Thank you for using our bank. Goodbye!");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+            System.out.println();
+        }
+    }
+}
--- src/UI/WelcomePage.java
+++ src/UI/WelcomePage.java
@@ -1,6 +1,9 @@
+package UI;
+
 import javax.swing.*;
 import java.awt.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class WelcomePage extends JFrame {
     // Constructor
@@ -22,20 +25,39 @@ public class WelcomePage extends JFrame {
 
         // Create a label with welcome text
         JLabel welcomeLabel = new JLabel("Welcome to Curry Bank");
-        JButton nextpageButton = new JButton("next page");
+        JButton nextPageButton = new JButton("Next Page");
+        nextPageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showLoginPage();
+            }
+        });
         welcomeLabel.setFont(new Font("Arial", Font.BOLD, 24));
         welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
-        // Add the label to the panel
+        // Add the label and button to the panel
         panel.add(welcomeLabel, BorderLayout.CENTER);
+        panel.add(nextPageButton, BorderLayout.SOUTH);
 
         // Add the panel to the frame
         add(panel);
     }
 
-    // Main method to start the application
+    private void showLoginPage() {
+        // Hide the current welcome page
+        setVisible(false);
+
+        // Create a new instance of the login page
+        LoginPage loginPage = new LoginPage();
+
+        // Dispose the current welcome page
+        dispose();
+
+        // Show the login page
+        loginPage.setVisible(true);
+    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new WelcomePage());
     }
 }
-
